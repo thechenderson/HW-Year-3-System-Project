f3cb25b85d1fe9734fcd06706bfcbde7ec323394
Jordan Low <jordan2311@hotmail.co.uk>
2020-01-16 14:18:37 +0000

    Jordan attempt

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   printf("Testing commit and VSCode");
+   printf("Testing commit and VSCode, Jordans is still fucked");
    return 0;
 }
 //Test commit

7fd585bc7bd0dde3b74986251d0c41be6aca1646
Jordan Low <jordan2311@hotmail.co.uk>
2020-01-23 15:49:35 +0000

    Testing time without sync

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -2,7 +2,7 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!\n");
-   // Added to test if my compiler has been set up correctly and if my commits update the GitHub code for everyone
+   // Added to test if my compiler has been set up correctly and if my commits update the GitHub code for everyone.
    printf("Testing run and compile, Jordan\n");
    return 0;
 }

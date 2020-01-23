cbf640a52ac84395fb56457110a2dbc25c9b5bdc
Jordan Low <jordan2311@hotmail.co.uk>
2020-01-23 15:37:06 +0000

    Testing compiler and GitHub commit - Jordan

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World!\n");
+   // Added to test if my compiler has been set up correctly and if my commits update the GitHub code for everyone
    printf("Testing run and compile, Jordan");
    return 0;
 }

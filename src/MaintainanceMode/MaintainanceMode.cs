dd64afd2b57ca0809d7d2b7290344399714e5072
Jordan Low <jordan2311@hotmail.co.uk>
2020-02-14 15:20:46 +0000

    Commented code and added distance text box

--- src/MaintainanceMode/MaintainanceMode.cs
+++ src/MaintainanceMode/MaintainanceMode.cs
@@ -38,6 +38,7 @@ namespace MaintainanceMode{
         private Button btnReadDistance;
         private Button btnReadColour;
         private PictureBox pbColour;
+        private TextBox tbDistance;
 
         //Variables
         int global_error;
@@ -47,44 +48,6 @@ namespace MaintainanceMode{
             InitializeComponent();
         }
 
-
-        public int CheckConnect()//Checking if there is a connection between the PC and an MBED
-        {
-            string Response;
-            int Status;
-
-            serialPort1.DiscardInBuffer();
-            serialPort1.ReadTimeout = READ_TIMEOUT;
-            try
-            {
-                Response = serialPort1.ReadLine();
-            }
-            catch (TimeoutException)
-            {
-                tbDisplay.AppendText("Readline timeout fail" + Environment.NewLine);
-                return -1;
-            }
-            Status = Convert.ToInt32(Response);
-            tbDisplay.AppendText("Status = " + Response + Environment.NewLine);
-            return Status;
-        }
-        public string ReadData()//Checking if there is a connection between the PC and an MBED
-        {
-            string Response;
-            serialPort1.DiscardInBuffer();
-            serialPort1.ReadTimeout = READ_TIMEOUT;
-            try
-            {
-                Response = serialPort1.ReadLine();
-                
-            }
-            catch (TimeoutException)
-            {
-                tbDisplay.AppendText("Readline timeout fail" + Environment.NewLine);
-                return "";
-            }
-            return Response;
-        }
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -105,6 +68,7 @@ namespace MaintainanceMode{
             this.btnReadDistance = new System.Windows.Forms.Button();
             this.btnReadColour = new System.Windows.Forms.Button();
             this.pbColour = new System.Windows.Forms.PictureBox();
+            this.tbDistance = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudServoNo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudServoAngle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbColour)).BeginInit();
@@ -295,10 +259,21 @@ namespace MaintainanceMode{
             this.pbColour.TabIndex = 15;
             this.pbColour.TabStop = false;
             // 
+            // tbDistance
+            // 
+            this.tbDistance.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDistance.Location = new System.Drawing.Point(72, 492);
+            this.tbDistance.Name = "tbDistance";
+            this.tbDistance.ReadOnly = true;
+            this.tbDistance.Size = new System.Drawing.Size(100, 50);
+            this.tbDistance.TabIndex = 16;
+            this.tbDistance.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // MaintainanceForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.ClientSize = new System.Drawing.Size(488, 853);
+            this.Controls.Add(this.tbDistance);
             this.Controls.Add(this.pbColour);
             this.Controls.Add(this.btnReadColour);
             this.Controls.Add(this.btnReadDistance);
@@ -343,17 +318,55 @@ namespace MaintainanceMode{
             Thread.Sleep(2000);
         }
 
-        private void btnConnect_Click(object sender, EventArgs e)
+        public int CheckConnect()//Checking if there is a connection between the PC and an MBED
         {
-            string com_port = cbPort.SelectedItem.ToString();
+            string Response;
+            int Status;
+
+            serialPort1.DiscardInBuffer();//Clearing any existing data from the connection between the PC and MBED
+            serialPort1.ReadTimeout = READ_TIMEOUT;//Setting the length for a Time Out
+            try
+            {
+                Response = serialPort1.ReadLine();//Reading the data from the MBED
+            }
+            catch (TimeoutException)//If there is an error recieving data
+            {
+                tbDisplay.AppendText("Readline timeout fail" + Environment.NewLine);
+                return -1;
+            }
+            Status = Convert.ToInt32(Response);
+            tbDisplay.AppendText("Status = " + Response + Environment.NewLine);//Displaying the MBED status
+            return Status;
+        }
+        public string ReadData()//Reading the values being passed from the MBED
+        {
+            string Response;//Setting a string to store the data that is read
+            serialPort1.DiscardInBuffer();//Removing all existing data from the connection
+            serialPort1.ReadTimeout = READ_TIMEOUT;//Setting the time out length
+            try
+            {
+                Response = serialPort1.ReadLine();//Reading the data from the MBED
+
+            }
+            catch (TimeoutException)//If there is an error reading the data
+            {
+                tbDisplay.AppendText("Readline timeout fail" + Environment.NewLine);
+                return "";//returns a blank string to stop the program from crashing
+            }
+            return Response;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)//When the connect button is pressed
+        {
+            string com_port = cbPort.SelectedItem.ToString();//Reading in the selected port from the drop down box
             tbDisplay.AppendText("Selected COM Port = " + com_port + Environment.NewLine);
             try
             {
                 tbDisplay.AppendText("Trying to open " + com_port + Environment.NewLine);
                 serialPort1.PortName = com_port;
-                serialPort1.Open();
+                serialPort1.Open();//Attempt to open up a connection on the selected COM port
             }
-            catch
+            catch //If no connection could be made
             {
                 tbDisplay.AppendText("Cannot open " + com_port + Environment.NewLine);
                 return;
@@ -361,43 +374,53 @@ namespace MaintainanceMode{
             tbDisplay.AppendText(com_port + " now open" + Environment.NewLine);
         }
 
-        private void btnServoMove_Click(object sender, EventArgs e)
+        private void btnServoMove_Click(object sender, EventArgs e)//When the Move Servo button is pressed
         {
+            //Setting the command to have 's' so the MBED knows to move the servo, the servo number and the position to move the servo to
             string command = "s "
                            + Convert.ToString(nudServoNo.Value)
                            + " "
                            + Convert.ToString(nudServoAngle.Value);
+
             tbDisplay.AppendText(command + Environment.NewLine);
-            serialPort1.WriteLine(command);
+            serialPort1.WriteLine(command);//Send the command to the MBED
             tbDisplay.AppendText("Reading reply" + Environment.NewLine);
-            int status = CheckConnect();
+            int status = CheckConnect();//Get the status from the MBED
         }
 
-        private void btnServoRead_Click(object sender, EventArgs e)
+        private void btnServoRead_Click(object sender, EventArgs e)//When the Read Servo button is pressed
         {
+            //Sends the command 'r' to the MBED to get the last servo value
             String command = "r";
             tbDisplay.AppendText(command + Environment.NewLine);
             serialPort1.WriteLine(command);
+
             tbDisplay.AppendText("Reading reply: " + Environment.NewLine);
-            int status = CheckConnect();
-            /*
-            // only read data if status reply was 0 (i.e. was successful)
-            */
-            if (status == 0)
+
+            string data = ReadData();//Getting the value from the MBED
+            int status = CheckConnect();//Getting the status from the MBED
+
+            if (status == 0)//Running if the MBED program ran correctly
             {
                 tbDisplay.AppendText("reading data" + Environment.NewLine);
-                string data = serialPort1.ReadLine();
                 tbDisplay.AppendText("Data = " + data + Environment.NewLine);
+            }
+            else //Displaying if there was an error when the MBED operated
+            {
+                tbDisplay.AppendText("Error when reading the data");
             }
         }
 
-        private void btnReadDistance_Click(object sender, EventArgs e)
+        private void btnReadDistance_Click(object sender, EventArgs e)//When the Read Distance button is pressed
         {
+
             String command = "d";
             tbDisplay.AppendText(command + Environment.NewLine);
             serialPort1.WriteLine(command);
             tbDisplay.AppendText("Reading reply: " + Environment.NewLine);
+            string distance = ReadData();
             int status = CheckConnect();
+
             /*
                 Only check for reply if status = 0 (was successful)
             */
@@ -410,29 +433,38 @@ namespace MaintainanceMode{
                 tbDisplay.AppendText("Data received: " + data + Environment.NewLine);
             }
             */
+            tbDistance.Text = distance;
         }
 
 
 
         private void btnReadColour_Click(object sender, EventArgs e)
         {
+
+            //Sending the command 'c' to the embed to initiate the colour sensor function
             String command = "c";
             tbDisplay.AppendText(command + Environment.NewLine);
             serialPort1.WriteLine(command);
+
             tbDisplay.AppendText("Reading reply: " + Environment.NewLine);
-            string clear = ReadData();
-            tbDisplay.AppendText("Clear Value: "+ clear + Environment.NewLine);
-            string red = ReadData();
-            tbDisplay.AppendText("Red Value: " + red + Environment.NewLine);
-            string green = ReadData();
-            tbDisplay.AppendText("Green Value: " + green + Environment.NewLine);
-            string blue = ReadData();
-            tbDisplay.AppendText("Blue Value: " + blue + Environment.NewLine);
-            this.pbColour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(Convert.ToInt32(red))))), ((int)(((byte)(Convert.ToInt32(green))))), ((int)(((byte)(Convert.ToInt32(blue))))));
+
+            string clear = ReadData();//Calling the ReadData function to get the response from the MBED which will be the clear value
+            tbDisplay.AppendText("Clear Value: "+ clear + Environment.NewLine);//Printing the clear value on the display
+            string red = ReadData();//Calling the ReadData function to get the response from the MBED which will be the red value
+            tbDisplay.AppendText("Red Value: " + red + Environment.NewLine);//Printing the red value on the display
+            string green = ReadData();//Calling the ReadData function to get the response from the MBED which will be the green value
+            tbDisplay.AppendText("Green Value: " + green + Environment.NewLine);//Printing the green value on the display
+            string blue = ReadData();//Calling the ReadData function to get the response from the MBED which will be the blue value
+            tbDisplay.AppendText("Blue Value: " + blue + Environment.NewLine);//Printing the blue value on the display
+
+            //Changing the colour box on the form to the colour from the sensor using the values obtained
+            pbColour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(Convert.ToInt32(red))))), ((int)(((byte)(Convert.ToInt32(green))))), ((int)(((byte)(Convert.ToInt32(blue))))));
+            
+            //Getting the status from the MBED to ensure the procedure ran correctly
             int status = CheckConnect();
 
         }
-        static void Main()
+        static void Main()//Starts the program and sets up the form
         {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);

96f24ede4056ada07b4b31470c0aa6a2d542f6f1
Patrick Geiger <patrick.geiger@epfl.ch>
2020-02-20 12:47:27 +0100

    Try to size controls for a better design.

--- UI/UI/TRANSLATION.Designer.cs
+++ UI/UI/TRANSLATION.Designer.cs
@@ -39,9 +39,11 @@
             // 
             // backtomm
             // 
-            this.backtomm.Location = new System.Drawing.Point(831, 1276);
+            this.backtomm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.backtomm.Location = new System.Drawing.Point(192, 879);
+            this.backtomm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.backtomm.Name = "backtomm";
-            this.backtomm.Size = new System.Drawing.Size(208, 71);
+            this.backtomm.Size = new System.Drawing.Size(156, 58);
             this.backtomm.TabIndex = 3;
             this.backtomm.Text = "Back to main menu";
             this.backtomm.UseVisualStyleBackColor = true;
@@ -49,9 +51,11 @@
             // 
             // sentence1
             // 
-            this.sentence1.Location = new System.Drawing.Point(48, 115);
+            this.sentence1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.sentence1.Location = new System.Drawing.Point(20, 113);
+            this.sentence1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sentence1.Name = "sentence1";
-            this.sentence1.Size = new System.Drawing.Size(311, 364);
+            this.sentence1.Size = new System.Drawing.Size(196, 276);
             this.sentence1.TabIndex = 4;
             this.sentence1.Text = "I am an alien robot";
             this.sentence1.UseVisualStyleBackColor = true;
@@ -59,9 +63,11 @@
             // 
             // sentence2
             // 
-            this.sentence2.Location = new System.Drawing.Point(491, 115);
+            this.sentence2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.sentence2.Location = new System.Drawing.Point(317, 113);
+            this.sentence2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sentence2.Name = "sentence2";
-            this.sentence2.Size = new System.Drawing.Size(311, 364);
+            this.sentence2.Size = new System.Drawing.Size(196, 276);
             this.sentence2.TabIndex = 6;
             this.sentence2.Text = "I like Haggis pizza";
             this.sentence2.UseVisualStyleBackColor = true;
@@ -69,9 +75,10 @@
             // 
             // sentence4
             // 
-            this.sentence4.Location = new System.Drawing.Point(491, 603);
+            this.sentence4.Location = new System.Drawing.Point(317, 579);
+            this.sentence4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sentence4.Name = "sentence4";
-            this.sentence4.Size = new System.Drawing.Size(311, 364);
+            this.sentence4.Size = new System.Drawing.Size(196, 276);
             this.sentence4.TabIndex = 7;
             this.sentence4.Text = "Come play with me!";
             this.sentence4.UseVisualStyleBackColor = true;
@@ -82,9 +89,10 @@
             this.fr_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.fr_button.BackgroundImage = global::UI.Properties.Resources.FR;
             this.fr_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.fr_button.Location = new System.Drawing.Point(813, 12);
+            this.fr_button.Location = new System.Drawing.Point(339, 11);
+            this.fr_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fr_button.Name = "fr_button";
-            this.fr_button.Size = new System.Drawing.Size(124, 76);
+            this.fr_button.Size = new System.Drawing.Size(93, 62);
             this.fr_button.TabIndex = 9;
             this.fr_button.UseVisualStyleBackColor = true;
             this.fr_button.Click += new System.EventHandler(this.fr_button_Click);
@@ -94,18 +102,21 @@
             this.eng_buttton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.eng_buttton.BackgroundImage = global::UI.Properties.Resources.ENG;
             this.eng_buttton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.eng_buttton.Location = new System.Drawing.Point(943, 12);
+            this.eng_buttton.Location = new System.Drawing.Point(436, 11);
+            this.eng_buttton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.eng_buttton.Name = "eng_buttton";
-            this.eng_buttton.Size = new System.Drawing.Size(124, 76);
+            this.eng_buttton.Size = new System.Drawing.Size(93, 62);
             this.eng_buttton.TabIndex = 8;
             this.eng_buttton.UseVisualStyleBackColor = true;
             this.eng_buttton.Click += new System.EventHandler(this.eng_buttton_Click);
             // 
             // sentence3
             // 
-            this.sentence3.Location = new System.Drawing.Point(48, 603);
+            this.sentence3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.sentence3.Location = new System.Drawing.Point(20, 579);
+            this.sentence3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sentence3.Name = "sentence3";
-            this.sentence3.Size = new System.Drawing.Size(311, 364);
+            this.sentence3.Size = new System.Drawing.Size(196, 276);
             this.sentence3.TabIndex = 10;
             this.sentence3.Text = "I was created by ArBots";
             this.sentence3.UseVisualStyleBackColor = true;
@@ -113,9 +124,9 @@
             // 
             // TRANSLATION
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1080, 1388);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(540, 960);
             this.Controls.Add(this.sentence3);
             this.Controls.Add(this.fr_button);
             this.Controls.Add(this.eng_buttton);
@@ -124,6 +135,7 @@
             this.Controls.Add(this.sentence1);
             this.Controls.Add(this.backtomm);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TRANSLATION";
             this.Text = "TRANSLATION";
             this.ResumeLayout(false);

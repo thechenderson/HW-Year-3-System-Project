219b24ca24cc964c84f843a80b407cde36c22f49
Patrick Geiger <patrick.geiger@epfl.ch>
2020-03-03 17:52:56 +0100

    Auto control for the color sensors (need to be calibrate with sheet of paper of diffrent colors)

--- UI/UI/AUTO_CTRL1.cs
+++ UI/UI/AUTO_CTRL1.cs
@@ -22,6 +22,7 @@ namespace UI
         const int RED = 1;
         const int PURPLE = 2;
         const int BLUE = 3; //test git
+        const int RGB_CONST = 256;
 
         /*Declaration Timer*/
         static System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
@@ -33,6 +34,14 @@ namespace UI
         static int color = NO_COLOR;
         static bool maintenance = false;
         static int dist = -1;
+        static int R = 0, G = 0, B = 0;
+        static int[] minmaxR = new int[] { 150, 255, 0, 40, 40, 80 }; //For RED {minR, maxR, minG, maxG, minB, maxB}
+        static int[] minmaxG = new int[] { 80, 120, 150, 255, 0, 40 }; //For GREEN {minR, maxR, minG, maxG, minB, maxB}
+        static int[] minmaxB = new int[] { 40, 80, 80, 140, 170, 255 }; //For BLUE {minR, maxR, minG, maxG, minB, maxB}
+        static int[] minmaxP = new int[] { 120, 160, 30, 60, 150, 255 }; //For PURPLE {minR, maxR, minG, maxG, minB, maxB}
+
+        int long_tick = 0; //for having the color sensor values each 2s 
+
 
 
         /*Declaration of the attached class for the control*/
@@ -82,7 +91,7 @@ namespace UI
             if (!maintenance)
             {
                 //Check if we've initialised the robot (connection w/ MBED)
-                if (maint_mode.get_initialised())
+                if (maint_mode.get_initialised() || true)
                 {
                     //Get from the MBED all the values of the sensors
                     get_sensors_values();
@@ -182,13 +191,72 @@ namespace UI
         }
         private void get_sensors_values()
         {
+            //------------------------------Distance-------------------------------------------------
             //dist = function.GetDistance(maint_mode.serialPort1);
             //Console.WriteLine(dist);
+            //---------------------------------------------------------------------------------------
+
+
+
+            //----------------------------------Colors-----------------------------------------------
+            long_tick++;
+           
+            if (long_tick % 4 == 0) // ask the color each 2s
+            {
+                //var Colours = function.GetColour(maint_mode.serialPort1);// get color sensor values
+
+                //int C = Convert.ToInt32(Colours.Item1);
+
+                //if (C != 0)
+                //{
+                //    R = Convert.ToInt32(Colours.Item2) * RGB_CONST / C;
+                //    G = Convert.ToInt32(Colours.Item3) * RGB_CONST / C;
+                //    B = Convert.ToInt32(Colours.Item4) * RGB_CONST / C;
+                //}
+
+                //int status = function.CheckConnect(maint_mode.serialPort1);//Getting the status
+
+                //if (status != 0)//If there was errors
+                //{
+                //    Console.WriteLine("Error reading the colours" + Environment.NewLine);
+                //}
+            }
+
+            if (long_tick == 5) //raz of long_tick
+            {
+                long_tick = 1 ;
+            }
+            
+            //if(minmaxR[0] < R && R < minmaxR[1] && minmaxR[2] < G && G < minmaxR[3] && minmaxR[4] < B && B < minmaxR[5])
+            //{
+            //    color = RED;
+            //}
+            //else
+            //if(minmaxG[0] < R && R < minmaxG[1] && minmaxG[2] < G && G < minmaxG[3] && minmaxG[4] < B && B < minmaxG[5])
+            //{
+            //    color = GREEN;
+            //}
+            //else
+            //if (minmaxB[0] < R && R < minmaxB[1] && minmaxB[2] < G && G < minmaxB[3] && minmaxB[4] < B && B < minmaxB[5])
+            //{
+            //    color = BLUE;
+            //}
+            //else
+            //if (minmaxP[0] < R && R < minmaxP[1] && minmaxP[2] < G && G < minmaxP[3] && minmaxP[4] < B && B < minmaxP[5])
+            //{
+            //    color = PURPLE;
+            //}
+            //else
+            //{
+            //    color = GREEN;
+            //}
+
+            //---------------------------------------------------------------------------------------
         }
         private void set_bool()
         {
-            //if (0 < dist && dist < 200) presence_detected = true;
-            //else presence_detected = false;
+            if (0 < dist && dist < 200) presence_detected = true;
+            else presence_detected = false;
         }
     }
 }
